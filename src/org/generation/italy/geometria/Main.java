034cae5c3c294a68877c00b20362a3fabbec646a
Sergio Minissale <minissale.sergio@outlook.itm>
2021-11-23 17:54:38 +0100

    esercizio + bonus while

--- src/org/generation/italy/geometria/Main.java
+++ src/org/generation/italy/geometria/Main.java
@@ -5,18 +5,30 @@ public class Main {
 	public static void main(String[] args) {
 		
 		Scanner scanner = new Scanner(System.in);
-		int altezza;
-		System.out.print("Altezza: ");
-		altezza = scanner.nextInt();
 		
-		int base;
-		System.out.print("Base: ");
-		base = scanner.nextInt();
+		int altezza = 0;
+		int base = 0;
 		
+		while (altezza <= 0) {
+			System.out.print("Altezza: ");
+			altezza = scanner.nextInt();
+			
+			if (altezza <= 0)
+				System.out.println("Valore non valido, inserire una altezza che sia > 0");
+		}
+		while (base <= 0) {
+			System.out.print("Base: ");
+			base = scanner.nextInt();
+			
+			if (altezza <= 0)
+				System.out.println("Valore non valido, inserire una base che sia > 0");
+		}
+
 		Rettangolo r1 = new Rettangolo(altezza, base);
 		r1.printRettangolo();
-		
+
+	
 		scanner.close();
 	}
-
 }
+
